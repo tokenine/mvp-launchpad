caca185496b619b7005cef45b10a365f5a055936
Teerapon Nampakdee <teerapon@kkumail.com>
2021-06-29 15:01:02 +0700

    first progress

--- src/constants/farms/bscTestnet.ts
+++ src/constants/farms/bscTestnet.ts
@@ -5,13 +5,13 @@ const farms: { [key: string]: any } = {
     masterchefAddress: '',
     pairAddress: '0x887930a46834d13b96856f271f10d3b309589f1c', // lp address
     token0: {
-      id: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+      address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
       name: 'Wrapped Ether',
       symbol: 'WETH',
       imageUrl: ''
     },
     token1: {
-      id: '0x9780881bf45b83ee028c4c1de7e0c168df8e9eef',
+      address: '0x9780881bf45b83ee028c4c1de7e0c168df8e9eef',
       name: 'Token USDC',
       symbol: 'USDC',
       imageUrl: ''
