3ee0ae2cd0c2c1972e3286a6398f7f7fc651a1f5
Dome C <dome@tel.co.th>
2021-07-04 16:36:02 +0700

    move to minichefV2

--- src/constants/farms/bscTestnet.ts
+++ src/constants/farms/bscTestnet.ts
@@ -1,20 +1,20 @@
 const farms: { [key: string]: any } = {
-  '0x887930a46834d13b96856f271f10d3b309589f1c': {
+  '0x14958Ff10E3Ee1981bF9622B0B79913150e01B35': {
     type: 'masterchefv1',
     pid: 1,
     masterchefAddress: '',
-    pairAddress: '0x887930a46834d13b96856f271f10d3b309589f1c', // lp address
-    symbol: 'USDC-ETH',
+    pairAddress: '0xefbd7858cd7a6932592573616f51dedc487b55dd', // lp address
+    symbol: 'TKA-TKB',
     token0: {
-      address: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
-      name: 'Wrapped Ether',
-      symbol: 'WETH',
+      address: '0xecafc0f1e5448868a08d89fa99e1d2a0694aee23',
+      name: 'TokenA',
+      symbol: 'TKA',
       imageUrl: ''
     },
     token1: {
-      address: '0x9780881bf45b83ee028c4c1de7e0c168df8e9eef',
-      name: 'Token USDC',
-      symbol: 'USDC',
+      address: '0x83c4cb8bea9e049a0d8df5f5ffc02563801e9e46',
+      name: 'TokenB',
+      symbol: 'TKB',
       imageUrl: ''
     }
   }
