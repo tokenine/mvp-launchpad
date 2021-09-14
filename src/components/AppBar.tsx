a4006b6d9f50b90baafead99cb0c641a9173a147
khing78 <sutiwat_o@kkumail.com>
2021-09-14 10:21:55 +0700

    change token and auction contract

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -133,9 +133,9 @@ function AppBar(): JSX.Element {
                                             <NavLink id={`bento-nav-link`} to={'/donate'}>
                                                 {i18n._(t`Pool for donation`)}
                                             </NavLink>
-                                            {/* <NavLink id={`bento-nav-link`} to={'/mev-auction'}>
+                                            <NavLink id={`bento-nav-link`} to={'/mev-auction'}>
                                                 {i18n._(t`MEV Auction`)}
-                                            </NavLink> */}
+                                            </NavLink>
                                             {/* {chainId === ChainId.MAINNET && (
                                                 <NavLink id={`vesting-nav-link`} to={'/vesting'}>
                                                     {i18n._(t`Vesting`)}
@@ -650,9 +650,9 @@ function AppBar(): JSX.Element {
                                 <NavLink id={`bento-nav-link`} to={'/donate'}>
                                     {i18n._(t`Pool for donation`)}
                                 </NavLink>
-                                {/* <NavLink id={`bento-nav-link`} to={'/mev-auction'}>
+                                <NavLink id={`bento-nav-link`} to={'/mev-auction'}>
                                     {i18n._(t`MEV Auction`)}
-                                </NavLink> */}
+                                </NavLink>
                                 {/* {chainId === ChainId.MAINNET && (
                                     <NavLink id={`stake-nav-link`} to={'/sushibar'}>
                                         {i18n._(t`SushiBar`)}
