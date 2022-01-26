3ed1ac2cf28b4979b7c577607a1ad4f038eabe05
Thanawat Permngram <thanawatpar@kkumail.com>
2022-01-26 12:29:21 +0700

    add mevtomvp in metachain

--- src/constants/mvptomev.ts
+++ src/constants/mvptomev.ts
@@ -1,13 +1,17 @@
 import { ChainId } from 'metaverse-sdk'
 
-export const NFT_AUCTION : { [key: string]: any } = {
-  [ChainId.BSC]: {
-    mev: '0x9b98646315CC7677CE02a3cCf580c80f36ACA4ff',
-    mvp: '0x3379A0BdF5A5CB566127C421782686BA0f80490a',
-  }
+export const NFT_AUCTION: { [key: string]: any } = {
+    [ChainId.BSC]: {
+        mev: '0x9b98646315CC7677CE02a3cCf580c80f36ACA4ff',
+        mvp: '0x3379A0BdF5A5CB566127C421782686BA0f80490a'
+    },
+    [ChainId.META]: {
+        mev: '0x9b98646315CC7677CE02a3cCf580c80f36ACA4ff',
+        mvp: '0x3379A0BdF5A5CB566127C421782686BA0f80490a'
+    }
 }
 
-export const useMVPToMEV = (chainId?: ChainId) : any => {
-  if (chainId === ChainId.BSC) return NFT_AUCTION[chainId]
-  return undefined
+export const useMVPToMEV = (chainId?: ChainId): any => {
+    if (chainId === ChainId.BSC || chainId === ChainId.META) return NFT_AUCTION[chainId]
+    return undefined
 }
