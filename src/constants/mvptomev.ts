e90d3799f964b57ea926be82c6a7cf9c3ef13d1d
Thanawat Permngram <thanawatpar@kkumail.com>
2022-01-26 12:49:36 +0700

    add add mev button app bar and auction in metachain

--- src/constants/mvptomev.ts
+++ src/constants/mvptomev.ts
@@ -6,8 +6,8 @@ export const NFT_AUCTION: { [key: string]: any } = {
         mvp: '0x3379A0BdF5A5CB566127C421782686BA0f80490a'
     },
     [ChainId.META]: {
-        mev: '0x9b98646315CC7677CE02a3cCf580c80f36ACA4ff',
-        mvp: '0x3379A0BdF5A5CB566127C421782686BA0f80490a'
+        mev: '0x70173C556183F17D4CE9690D44b77F7a1D476929',
+        mvp: '0x9737e3Be617d482cFCF013358e1DEB188aB63E0B'
     }
 }
 
